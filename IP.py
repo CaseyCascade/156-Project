92681322c237c304e9e0df38b0d80248f13fbd09
Skeletonman59 <Skeletonman59@gmail.com>
2024-12-09 15:39:34 -0800

    updating to casey branch

--- IP.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
-
-print(f"Your IP address is: {ip_address}")
--- TinkerAttmpt.py
+++
@@ -1,6 +0,0 @@
-import webbrowser
-import tkinter as tk
-root = tk.Tk()
-#root.colorchooser.askcolor(color=None, **options)
-root.mainloop()
-    
